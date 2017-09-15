4890258359dacf98508f7d4ea4d0440e6981e2a3
chunyiwu <31670173+chunyiwu@users.noreply.github.com>
2017-09-15 08:32:40 -0500

    Add files via upload

--- driver.m
+++ driver.m
@@ -1,24 +1,26 @@
 clear; clc; close all hidden;
 
-sma = 0.449 * OrbitParam.AU;
+sma = 0.449 * PlanetarySystemParam.AU;
 ecc = 0.9336;
-inc = 89.285 * OrbitParam.deg;
-lan = 160.98 * OrbitParam.deg;
-aop = 300.651 * OrbitParam.deg;
-tp  = OrbitParam.juldate2datenum(2454424.857);
+inc = 89.285 * PlanetarySystemParam.deg;
+lan = 160.98 * PlanetarySystemParam.deg;
+aop = 300.651 * PlanetarySystemParam.deg;
+tp  = PlanetarySystemParam.juldate2datenum(2454424.857);
 
-P = 111.43637 * OrbitParam.day;
-m1 = 4*pi^2*sma^3 / P^2 / OrbitParam.G;
+P = 111.43637 * PlanetarySystemParam.day;
+m1 = 4*pi^2*sma^3 / P^2 / PlanetarySystemParam.G;
 
-m2 = 3.94 * OrbitParam.M_Jup;
+m2 = 3.94 * PlanetarySystemParam.M_Jup;
 
-R1 = 0.98 * OrbitParam.R_Sun;
+R1 = 0.98 * PlanetarySystemParam.R_Sun;
 R2 = 0.098 * R1;
 
-r = 58.4 * OrbitParam.pc;
+rStar = 58.4 * PlanetarySystemParam.pc;
+RA  = PlanetarySystemParam.hms2deg(09,22,37.0);
+dec = PlanetarySystemParam.dms2deg(50,36,13);
 
 
-op = PlanetarySystemParam(sma,ecc,inc,lan,aop,tp,m1,m2,R1,R2,r,...
+op = PlanetarySystemParam(sma,ecc,inc,lan,aop,tp,m1,m2,R1,R2,rStar,RA,dec, ...
     'HD 80606','HD 80606 b');
 
 % op.plotRelAstrometry(1);
